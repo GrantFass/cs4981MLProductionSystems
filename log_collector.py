7fa01496881c2879af630ccec8d31fbebe6f2c71
Grant Fass <grantfass@gmail.com>
2022-12-14 14:21:54 -0600

    Get Log Collection Mostly Working
    - added timing to repeat log collection
    - moved file path to env file
    - implemented sending the json log file data to the minio server
    - set up log name schema

--- log_collector.py
+++ log_collector.py
@@ -3,22 +3,13 @@ import boto3
 from minio import Minio
 import os
 from dotenv import load_dotenv
+import json
+from datetime import datetime, timedelta
+from time import sleep
 
 load_dotenv()
-log_file = os.path.relpath('./log_file.json')
+diff = 1 # TODO: Read from cmd
 
-tail = Pygtail(log_file, save_on_end=True, copytruncate=False)
-count = 0
-temp = []
-for line, offset in tail.with_offsets():
-    temp.append(line)
-    count = offset
-print(temp)
-
-# client = Minio(
-#     os.getenv('ENDPOINT_URL'), 
-#     os.getenv('AWS_ACCESS_KEY_ID'), 
-#     os.getenv('AWS_SECRET_ACCESS_KEY'))
 s3_target = boto3.resource('s3', 
     endpoint_url=os.getenv('ENDPOINT_URL'),
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
@@ -27,6 +18,22 @@ s3_target = boto3.resource('s3',
     config=boto3.session.Config(signature_version='s3v4'),
     verify=False
 )
-print(s3_target)
-#tail.write_offset_to_file(count)
+
+while True:
+    start = datetime.now()
+    until = start + timedelta(minutes=diff)
+    while datetime.now() < until:
+        sleep(1)
+        
+    tail = Pygtail(os.getenv('LOG_PATH'), save_on_end=True, copytruncate=False)
+    temp = []
+    for line, offset in tail.with_offsets():
+        j = json.loads(line)
+        temp.append(j)
+    if temp:
+        temp = json.dumps(temp, default = lambda x: x.__dict__)
+        
+        log_name = "log_file_%s.json" % datetime.now().strftime("%Y%m%d-%H%M%S")
+        s3_target.Bucket('log-files').put_object(Key=log_name, Body=json.dumps(temp))
+    #tail.write_offset_to_file(count)
 
